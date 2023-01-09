48f205bb15a6f92845e0c0c915b3640390b7ed51
Kayxiii <jiaxuann1101@gmail.com>
2023-01-09 14:59:55 +0800

    Change income picture.

--- app/src/main/java/com/example/personalbudgetingapp/BudgetActivity.java
+++ app/src/main/java/com/example/personalbudgetingapp/BudgetActivity.java
@@ -307,7 +307,7 @@ public class BudgetActivity extends AppCompatActivity {
             @NonNull
             @Override
             public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_budgetlist, parent, false);
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.retrieve_budget_layout, parent, false);
                 return new MyViewHolder(view);
             }
         };
